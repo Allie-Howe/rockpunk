7b82fc77488d5be2816c46bea0191ecfd587936d
Allie Howe <howeisallie@gmail.com>
2023-12-24 21:26:18 +0000

    blue background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Nav } from './Nav';
+import Hero from './Hero';
+
 export default function Main() {
-  return <main className="flex min-h-screen flex-col">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ullam corporis maxime laboriosam nihil nesciunt aliquam, dicta magni illum exercitationem, natus culpa facilis ducimus perspiciatis, dolor eligendi quo. Nemo, a.</p>
+  return <main className="flex min-h-screen flex-col bg-blue-100">
+    <Nav />
+    <Hero />
   </main>
 }
