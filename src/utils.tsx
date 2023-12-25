93c67e775afa3e571173e984317a1ca0fcce3da0
Allie Howe <howeisallie@gmail.com>
2023-12-25 00:41:20 +0000

    regular quotes?

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,1 +1,1 @@
-export const Header = ({title}: {title: string}) => <h2 className={`text-6xl text-blue-500 mb-10`}>{title}</h2>
+export const Header = ({title}: {title: string}) => <h2 className='text-6xl text-blue-500 mb-10'>{title}</h2>
