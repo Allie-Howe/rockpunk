dba7eb3d909d7c180748733eadcf3ad35cb43507
Allie Howe <howeisallie@gmail.com>
2024-01-06 11:25:45 +0000

    added location area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,46 @@ import Hero from './Hero';
 import InstagramIcon from 'mdi-react/InstagramIcon'
 import FacebookIcon from 'mdi-react/FacebookIcon'
 import { MdiReactIconProps } from 'mdi-react';
+import { PropsWithChildren } from 'react';
+import { belanosima } from './fonts';
 
 const responsiveContainer = 'px-3 md:px-10'
 
 export default function Main() {
   return <main className={`flex min-h-screen flex-col`}>
     <Hero />
-    <RockSection />
+    <RockSection>
+      <div className='grid gap-5 md:grid-cols-2'>
+        <div>
+          <h3 className={`text-8xl ${belanosima.className}`}>location</h3>
+          <div className='text-gray-200'>
+            <p>26A Magdalen Street</p>
+            <p>Norwich</p>
+            <p>NR3 1HU</p>
+          </div>
+        </div>
+        <div className='bg-red-400 md:h-auto h-56'>
+          <div className='h-full flex justify-center items-center'>
+            map goes here
+          </div>
+        </div>
+      </div>
+    </RockSection>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit ducimus laborum iusto, similique odit quos tempore placeat atque ab illum aspernatur? Numquam sint itaque voluptates aperiam sit ratione repudiandae obcaecati.
+    <RockSection>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit ducimus laborum iusto, similique odit quos tempore placeat atque ab illum aspernatur? Numquam sint itaque voluptates aperiam sit ratione repudiandae obcaecati.
+    </RockSection>
     <Footer />
   </main>
 }
 
-const RockSection = () => {
+const RockSection = ({children}: PropsWithChildren) => {
   return (<>
     <div className='bg-red-500'>
       <svg id="visual" viewBox="0 0 900 350" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1"><path d="M0 310L225 195L450 316L675 97L900 245L900 0L675 0L450 0L225 0L0 0Z" fill="#1f2937" strokeLinecap="square" strokeLinejoin="bevel"></path></svg>
     </div>
     <div className={`bg-red-500 ${responsiveContainer}`}>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit ducimus laborum iusto, similique odit quos tempore placeat atque ab illum aspernatur? Numquam sint itaque voluptates aperiam sit ratione repudiandae obcaecati.
+      {children}
     </div>
     <svg className='-translate-y-1' id="visual" viewBox="0 0 900 350" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1"><path d="M0 310L225 195L450 316L675 97L900 245L900 0L675 0L450 0L225 0L0 0Z" fill="#ef4444" strokeLinecap="square" strokeLinejoin="bevel"></path></svg>
   </>)
